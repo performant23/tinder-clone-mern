f754d0a66b1a44ae36117d554dd98085d81b09e5
performant23 <rutam.kathale@gmail.com>
2023-07-04 12:41:28 +0530

    Updated code with working hosted backend

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -8,7 +8,7 @@ function Header() {
   return (
     <div className='header'>
     <IconButton><PersonIcon fontSize='large' className='header__icon'/></IconButton>
-    <img className='header__logo' src='https://1000logos.net/wp-content/uploads/2018/07/tinder-emblem.jpg'/>
+    <img className='header__logo' src='https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon-4.png'/>
     <IconButton><ChatBubbleOutlineIcon fontSize='large' className='header__icon'/></IconButton>
     </div>
   )
